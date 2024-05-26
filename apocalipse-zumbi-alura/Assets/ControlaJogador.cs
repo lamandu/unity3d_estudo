4ec172688de64d7d36c97ace7edadc66430e2142
Ricardo <rsouza.consultoria@gmail.com>
2024-05-26 03:03:10 -0300

    adicionando player com movimentacao basica

---
+++ apocalipse-zumbi-alura/Assets/ControlaJogador.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class ControlaJogador : MonoBehaviour
+{
+    public float velocidade = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float eixoX = Input.GetAxis("Horizontal");
+        float eixoZ = Input.GetAxis("Vertical");
+
+        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+
+        transform.Translate(direcao * velocidade * Time.deltaTime);
+    }
+}
