2151090cfc57c1c0387884e05f434555aeeba868
Ricardo <rsouza.consultoria@gmail.com>
2024-06-05 01:12:17 -0300

    ajuste colisao do jogador

--- apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
+++ apocalipse-zumbi-alura/Assets/Scripts/ControlaJogador.cs
@@ -8,9 +8,10 @@ public class ControlaJogador : MonoBehaviour
 {
     public float velocidade = 10;
     private float limiarMovimento = 0.1f; // Limiar para considerar como movimento
+    Vector3 direcao;
 
-    // Start is called before the first frame update
-    void Start()
+   // Start is called before the first frame update
+   void Start()
     {
 
     }
@@ -21,17 +22,25 @@ public class ControlaJogador : MonoBehaviour
         float eixoX = Input.GetAxis("Horizontal");
         float eixoZ = Input.GetAxis("Vertical");
 
-        Vector3 direcao = new Vector3(eixoX, 0, eixoZ);
+        direcao = new Vector3(eixoX, 0, eixoZ);
 
         // Verifica se a magnitude da direção é maior que o limiar de movimento
         if (direcao.magnitude > limiarMovimento)
         {
-            transform.Translate(direcao * velocidade * Time.deltaTime);
+           // transform.Translate(direcao * velocidade * Time.deltaTime);
             GetComponent<Animator>().SetBool("movendo", true);
         }
         else
         {
             GetComponent<Animator>().SetBool("movendo", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (direcao.magnitude > limiarMovimento)
+        {
+            GetComponent<Rigidbody>().MovePosition(GetComponent<Rigidbody>().position + (direcao * velocidade * Time.deltaTime));
         }
     }
 }
