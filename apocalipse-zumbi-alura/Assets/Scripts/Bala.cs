11a9a77817655ef48a621f5df0c961e98875b86f
Ricardo <rsouza.consultoria@gmail.com>
2024-06-07 01:07:12 -0300

    jogador atirando

---
+++ apocalipse-zumbi-alura/Assets/Scripts/Bala.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bala : MonoBehaviour
+{
+    public float Velocidade = 20;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        GetComponent<Rigidbody>().MovePosition
+                    (GetComponent<Rigidbody>().position + 
+                    transform.forward * Velocidade * Time.deltaTime);
+    }
+}
