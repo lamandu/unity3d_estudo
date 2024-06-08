8daa7e38957c2eef95d97390aca0c9fbcca2650c
Ricardo <rsouza.consultoria@gmail.com>
2024-06-08 13:14:46 -0300

    criando zumbis dinamicamente

---
+++ apocalipse-zumbi-alura/Assets/Scripts/GeradorZumbis.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeradorDeZumbis : MonoBehaviour
+{
+ 
+    public GameObject Zumbi;
+    float contadorTempo = 0;
+
+    public float TempoGerarZumbi = 2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        contadorTempo += Time.deltaTime;
+        if(contadorTempo>=TempoGerarZumbi) 
+        {
+            Instantiate(Zumbi, transform.position,transform.rotation);
+            contadorTempo = 0;
+        }
+    }
+}
